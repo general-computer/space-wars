5dbd46533111c2e445227bbbfe05fbb68dab0564
NokTNL <noklamtsz@gmail.com>
2022-04-29 22:28:21 +0100

    Initialise React app, create map grid

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import cl from "./App.module.css";
+
+const TANK_DATA = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+function App() {
+  return (
+    <div className={cl.App}>
+      <div className={cl.map}>
+        <TransformWrapper>
+          <TransformComponent>
+            <div className={cl.grid}>
+              {TANK_DATA.map((data) => (
+                <div className={`${cl.cell} ${data ? cl.hasTank : ""}`}></div>
+              ))}
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/store/exampleSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const exampleSlice = createSlice({
+  name: "example",
+  initialState: {},
+  reducers: {
+    example(state, action) {},
+  },
+});
+
+export default exampleSlice;
