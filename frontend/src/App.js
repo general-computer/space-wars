218ad0e50400401364869a36fe8db837a9e0679b
NokTNL <noklamtsz@gmail.com>
2022-04-30 22:24:32 +0100

    Make columns/rows number dynamic
    - Map dimension controlled by MAP_DIMENSION in App.js
    - Spaceship data is now random
    - Added "styled-components" as dependency and the Grid component is
      controlled by it now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import Map from "./components/Map";
 import cl from "./App.module.css";
 
-const TANK_DATA = [
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-];
+// Generates random 0 or 1
+const flipcoin = () => {
+  return Math.floor(Math.random() * 2);
+};
+
+// Generate random sample tank data
+const MAP_DIMENSION = 100;
+const SPACESHIP_DATA = (function () {
+  const arr = [];
+  for (let i = 0; i < MAP_DIMENSION ** 2; i++) {
+    arr.push(flipcoin());
+  }
+  return arr;
+})();
 
 function App() {
   return (
     <div className={cl.App}>
-      <Map data={TANK_DATA} />
+      <Map data={SPACESHIP_DATA} dim={MAP_DIMENSION} />
     </div>
   );
 }
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,17 +1,36 @@
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import styled from "styled-components";
 import cl from "./Map.module.css";
 
-export default function Map({ data }) {
+const Grid = styled.div`
+  background-color: white;
+  border: 0.05rem white solid;
+
+  /* Make the whole grid fits its .fuldimWrapper parent */
+  width: min(70vw, 70vh);
+  height: min(70vw, 70vh);
+
+  display: grid;
+  /* "gap" doesn't work well when dimension of the map is large */
+  /* gap: 0.1rem; */
+  grid-template-columns: repeat(${({ dim }) => dim}, 1fr);
+  grid-template-rows: repeat(${({ dim }) => dim}, 1fr);
+`;
+
+export default function Map({ data, dim }) {
   return (
     <div className={cl.map}>
       <TransformWrapper>
         <TransformComponent>
-          <div className={cl.fullSizeWrapper}>
-            <div className={cl.grid}>
-              {data.map((data) => (
-                <div className={`${cl.cell} ${data ? cl.hasTank : ""}`}></div>
+          <div className={cl.fuldimWrapper}>
+            <Grid dim={dim}>
+              {data.map((data, id) => (
+                <div
+                  className={`${cl.cell} ${data ? cl.hasTank : ""}`}
+                  key={id}
+                ></div>
               ))}
-            </div>
+            </Grid>
           </div>
         </TransformComponent>
       </TransformWrapper>
