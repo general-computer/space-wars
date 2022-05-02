1c2499c245e993459c66f1010d0dd2a9b5b0205f
NokTNL <noklamtsz@gmail.com>
2022-05-02 21:24:56 +0100

    Generate 69 random spaceship positions
    Creates empty array in advance instead of querying random number
    generator for each map cell for speeding up rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
 import Map from "./components/Map";
 import cl from "./App.module.css";
 
-// Generates random 0 or 1
-const flipcoin = () => {
-  return Math.floor(Math.random() * 2);
+/******** Generate random sample tank data *****/
+const MAP_LENGTH = 100;
+const NUM_OF_SPACESHIPS = 69;
+
+const randXYArr = () => {
+  const x = Math.floor(Math.random() * MAP_LENGTH);
+  const y = Math.floor(Math.random() * MAP_LENGTH);
+  return [x, y];
 };
 
-// Generate random sample tank data
-const MAP_DIMENSION = 100;
 const SPACESHIP_DATA = (function () {
   const arr = [];
-  for (let i = 0; i < MAP_DIMENSION ** 2; i++) {
-    arr.push(flipcoin());
+  for (let i = 0; i < NUM_OF_SPACESHIPS; i++) {
+    arr.push(randXYArr());
   }
   return arr;
 })();
+/* ***************************************  */
 
 function App() {
   return (
     <div className={cl.App}>
-      <Map data={SPACESHIP_DATA} dim={MAP_DIMENSION} />
+      <Map data={SPACESHIP_DATA} len={MAP_LENGTH} />
     </div>
   );
 }
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -6,30 +6,52 @@ const Grid = styled.div`
   background-color: white;
   border: 0.05rem white solid;
 
-  /* Make the whole grid fits its .fuldimWrapper parent */
+  /* Make the whole grid fits its .fullSizeWrapper parent */
   width: min(70vw, 70vh);
   height: min(70vw, 70vh);
 
   display: grid;
   /* "gap" doesn't work well when dimension of the map is large */
   /* gap: 0.1rem; */
-  grid-template-columns: repeat(${({ dim }) => dim}, 1fr);
-  grid-template-rows: repeat(${({ dim }) => dim}, 1fr);
+  grid-template-columns: repeat(${({ len }) => len}, 1fr);
+  grid-template-rows: repeat(${({ len }) => len}, 1fr);
 `;
 
-export default function Map({ data, dim }) {
+export default function Map({ data, len }) {
+  // An empty len*len array for rendering the map
+  const cellArray = (function () {
+    const yxArray = [];
+    for (let y = 0; y < len; y++) {
+      const yArray = [];
+      for (let x = 0; x < len; x++) {
+        yArray.push(0);
+      }
+      yxArray.push(yArray);
+    }
+    return yxArray;
+  })();
+
+  // Filling in cellArray with data
+  for (const spaceship of data) {
+    const [x, y] = spaceship;
+    console.log(x, y);
+    cellArray[y][x] = 1;
+  }
+
   return (
     <div className={cl.map}>
       <TransformWrapper>
         <TransformComponent>
-          <div className={cl.fuldimWrapper}>
-            <Grid dim={dim}>
-              {data.map((data, id) => (
-                <div
-                  className={`${cl.cell} ${data ? cl.hasTank : ""}`}
-                  key={id}
-                ></div>
-              ))}
+          <div className={cl.fullSizeWrapper}>
+            <Grid len={len}>
+              {cellArray.map((yArray) =>
+                yArray.map((data, id) => (
+                  <div
+                    className={`${cl.cell} ${data === 1 ? cl.hasTank : ""}`}
+                    key={id}
+                  ></div>
+                ))
+              )}
             </Grid>
           </div>
         </TransformComponent>
