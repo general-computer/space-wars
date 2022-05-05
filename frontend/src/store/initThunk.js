9625eace2ab1195f2f5baae0b7277c8446df6e5c
NokTNL <noklamtsz@gmail.com>
2022-05-05 19:00:58 +0100

    Check wallet exists before loading data

--- frontend/src/store/initThunk.js
+++ frontend/src/store/initThunk.js
@@ -3,15 +3,29 @@ import walletSlice from "./walletSlice";
 
 export default function init() {
   return async (dispatch) => {
-    // Listen to account changes in wallet
-    window.ethereum.on("accountsChanged", (accounts) => {
-      if (accounts[0] !== undefined) {
-        dispatch(walletSlice.actions.changeWalletStatus({ addr: accounts[0] }));
-      } else {
-        dispatch(walletSlice.actions.changeWalletStatus({ addr: "" }));
-      }
-    });
+    try {
+      const { changeWalletStatus } = walletSlice.actions;
 
-    dispatch(loadData());
+      // Check: the window object must have ethereum injected
+      if (!window?.ethereum) {
+        alert(
+          "Have an Ethereum wallet (like Metamask) installed and enabled in the browser to use this app!"
+        );
+        dispatch(changeWalletStatus({ addr: "No wallet!" }));
+        throw new Error("Spaceship: window.ethereum does not exist");
+      }
+      // Listen to account changes in wallet
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts[0] !== undefined) {
+          dispatch(changeWalletStatus({ addr: accounts[0] }));
+        } else {
+          dispatch(changeWalletStatus({ addr: "" }));
+        }
+      });
+
+      dispatch(loadData());
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 }
--- frontend/src/store/userActionThunk.js
+++ frontend/src/store/userActionThunk.js
@@ -4,16 +4,14 @@ export default function userAction({ type }) {
   return async (dispatch) => {
     switch (type) {
       case "CONNECT":
-        if (window?.ethereum) {
-          const accounts = await window.ethereum.request({
-            method: "eth_requestAccounts",
-          });
-          dispatch(
-            walletSlice.actions.changeWalletStatus({
-              addr: accounts[0],
-            })
-          );
-        } else alert("Ethereum is not enabled in the browser.");
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        dispatch(
+          walletSlice.actions.changeWalletStatus({
+            addr: accounts[0],
+          })
+        );
         break;
       default:
         throw new Error(`userAction(): unknown action type`);
