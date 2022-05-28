3e386d409576e0b3898f263bb47ea50f63964db2
Kibouweb3 <100516482+Kibouweb3@users.noreply.github.com>
2022-05-28 04:39:04 +0300

    Updated the human-readable ABI

--- frontend/src/contract/gameABI.js
+++ frontend/src/contract/gameABI.js
@@ -3,8 +3,9 @@ const UnitData = `(
     int56 y,
     uint8 level,
     uint8 lives,
-    uint64 points,
+    uint56 points,
     uint56 lastSimulatedDay,
+    uint8 colorSeed,
 )`;
 
 const SUPPLY = 69;
@@ -13,7 +14,7 @@ const gameABI = [
   "event UnitMoved(uint256 tokenId, int56 x, int56 y)",
   "event UnitShot(uint256 attId, uint256 victId, uint8 damage)",
   "event UnitUpgraded(uint256 tokenId, uint8 level)",
-  "event UnitGavePoints(uint256 fromTokenId, uint256 toTokenId, uint64 amount)",
+  "event UnitGavePoints(uint256 fromTokenId, uint256 toTokenId, uint56 amount)",
   // From ERC721A: https://chiru-labs.github.io/ERC721A/#/erc721a?id=events
   "event Transfer(address from, address to, uint256 tokenId)",
 
@@ -35,7 +36,7 @@ const gameABI = [
   `function getState() public view returns(uint256 currentZoneRadius, uint256 s_gameStartTime, ${UnitData}[${SUPPLY}] memory allUnits, string[${SUPPLY}] memory images)`,
   `function move(uint256 unit, int56 x, int56 y) public`,
   `function shoot(uint256 attId, uint256 victId, uint8 damage) public`,
-  `function givePoints(uint256 fromId, uint256 toId, uint64 amount) public`,
+  `function givePoints(uint256 fromId, uint256 toId, uint56 amount) public`,
   `function upgrade(uint256 unitId, uint8 byLevels) public`,
   `function imageURI(uint256 _tokenId) public pure returns (string memory)`,
   `function tokenURI(uint256 _tokenId) public view returns (string memory)`,
